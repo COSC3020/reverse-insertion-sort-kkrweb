39d31e923938c0ef34e0403535e38dde27586672
Kane Kriz <kanemkriz@gmail.com>
2025-01-30 16:18:34 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
-function insertionSortReverse(array) {
-    return array;
+// Kane Kriz
+// UWYO COSC 3020
+// 30 January 2025
+
+function insertionSortReverse(array) 
+{
+  for(var i = array.length-1; i > 0; i--) 
+  {
+    var val = array[i];
+    var j;
+      
+    for(j = i; j < array.length-1 && arr[j+1] < val; j++) 
+    {
+      array[j] = array[j+1];
+    }
+      
+    array[j] = val;
+  }
+  return array;
 }
