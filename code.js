6f67dfb847a2a3bf75059b6e30142f7abb1534cc
Kane Kriz <kanemkriz@gmail.com>
2025-01-30 16:22:37 -0700

    Update code.js

--- code.js
+++ code.js
@@ -6,15 +6,15 @@ function insertionSortReverse(array)
 {
   for(var i = array.length-1; i > 0; i--) 
   {
-    var val = array[i];
+    var val = array[i-1];
     var j;
       
-    for(j = i; j < array.length-1 && arr[j+1] < val; j++) 
+    for(j = i; j < array.length && array[j] < val; j++) 
     {
-      array[j] = array[j+1];
+      array[j-1] = array[j];
     }
       
-    array[j] = val;
+    array[j-1] = val;
   }
   return array;
 }
